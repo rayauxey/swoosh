03da404f662e2ae019d6c607d16a69c4ca78ed5f
Ray Auxey <rayauxey@gmail.com>
2018-04-05 20:56:24 +0300

    Initial commit

--- app/src/main/java/com/raymediax/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/raymediax/swoosh/LeagueActivity.kt
@@ -1,12 +1,18 @@
 package com.raymediax.swoosh
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BasicActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+    fun leagueNextClicked(view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
+    }
+
 }
---
+++ app/src/main/java/com/raymediax/swoosh/SkillActivity.kt
@@ -0,0 +1,15 @@
+package com.raymediax.swoosh
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+
+}
